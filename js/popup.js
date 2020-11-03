d80d535fcc483356272ba2e2957cd2ba8c4f0fda
Dominic789654 <47224289+Dominic789654@users.noreply.github.com>
2020-11-03 18:21:54 -0500

    1

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,39 @@
+(function(){
+  var storage = chrome.storage;
 
+  function updateIconAndText() {
+    storage.local.get("notificationStatus", function(item){
+      if(item.notificationStatus === "block"){
+        boxText.innerText = "Focus Mode On";
+        chrome.browserAction.setIcon({'path':'../images/icon_white.png'});
+      }
+      else {
+        boxText.innerText = "Focus Mode Off";
+        chrome.browserAction.setIcon({'path':'../images/icon_black.png'});
+      }
+    });
+  }
+
+  function onCheckboxClick(){
+    var cBox = document.getElementById('switch_1');
+    if (cBox.checked){
+      storage.local.set({"notificationStatus": "block"});
+      chrome.contentSettings['notifications'].set({
+        'primaryPattern':'<all_urls>',
+        'setting':'block'
+      });
+    }
+    else{
+      storage.local.set({"notificationStatus":"none"});
+      chrome.contentSettings['notifications'].clear({});
+    }
+    updateIconAndText();
+  }
+
+
+  updateIconAndText();
+
+
+  var cBox = document.getElementById('switch_1');
+  cBox.addEventListener("click",onCheckboxClick);
+})();
