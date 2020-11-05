ccc55cf7ebb3560c956ad2d811829424e8415255
Dominic789654 <47224289+Dominic789654@users.noreply.github.com>
2020-11-05 17:50:41 -0500

    timer version 1.0

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,46 @@
-var oBox = document.getElementById('switch_1');
-function updateIconAndText(){
-  var boxText = document.getElementById("boxText");
+function startTimer(duration, display) { //statr timer count down
+    var timer = duration, hours, minutes, seconds;
+    var countDown = setInterval(function () {
+        hours = parseInt(timer / 3600, 10);
+        minutes = parseInt((timer/60) % 60, 10);
+        seconds = parseInt(timer % 60,10);
 
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = hours + ":" + minutes+ ":" +seconds;
+
+        if (--timer < 0) {
+            clearInterval(countDown);
+            chrome.storage.local.set({'status':'none'});
+        }
+    }, 1000);
+    updateIconAndText();
+}
+
+var submitButton = document.getElementById("submitButton");
+var hour = document.getElementById("hour");
+var minute = document.getElementById("minute");
+var cancelButton = document.getElementById("cancelButton");
+var oBox = document.getElementById('switch_1');
+submitButton.onclick = function () { // when click submit button
+    var inputSeconds = 60*((60 * hour.value)+minute.value);
+    display = document.querySelector('#time');
+    chrome.storage.local.set({'status':'block'});
+    startTimer(inputSeconds, display);
+    updateIconAndText();
+};
+
+cancelButton.onclick = function() { // when click cancel button
+    chrome.storage.local.set({'status':'none'});
+    var inputSeconds = 0;
+    display = document.querySelector('#time');
+    startTimer(inputSeconds, display);
+    updateIconAndText();
+}
+
+function updateIconAndText(){ // when status change, update icon and text
+  var boxText = document.getElementById("boxText");
   chrome.storage.local.get("status", function(item){
     if(item.status == 'block'){
       boxText.innerText = "Focus Mode On";
@@ -25,7 +64,7 @@ function updateIconAndText(){
 }
 
 
-oBox.onclick = function(){
+oBox.onclick = function(){ // click check box
   if (this.checked){
     chrome.storage.local.set({'status':'block'});
     chrome.contentSettings['notifications'].set({
@@ -39,4 +78,5 @@ oBox.onclick = function(){
   }
   updateIconAndText();
 }
+
 updateIconAndText();
