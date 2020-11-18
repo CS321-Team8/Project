2264160cbf7f60b0e62b2161927d0e87945d151a
cazurbach <59846812+cazurbach@users.noreply.github.com>
2020-11-18 13:25:08 -0500

    Update block_url.js

--- js/block_url.js
+++ js/block_url.js
@@ -16,3 +16,16 @@ const webRequestFlags = [
   filter,
   webRequestFlags,
 );
+//BLOCK THE ENTIRE DOMAIN WITH THE FOLLOWING FUNCTION
+findAllURL = function changeAllURL(text){
+  var current = window.location.href;
+  if(current.startsWith(text)){
+    document.documentElement.innerHTML = '';
+    document.documentElement.innerHTML = 'Domain is blocked';
+    document.documentElement.scrollTop = 0;
+  }
+}
+
+
+findURL("https://www.twitter.com/");
+findAllURL("https://www.facebook.com/");
