4bcd9a7bc947320ce91443f62239c077fdc9dabd
cazurbach <59846812+cazurbach@users.noreply.github.com>
2020-11-18 14:00:02 -0500

    Update block_url.js

--- js/block_url.js
+++ js/block_url.js
@@ -1,41 +1,4 @@
-const filter = {
-  urls: [
-    '*://twitter.com/*', '*://facebook.com/*', '*://youtube.com/*'
-  ],
-}
-const webRequestFlags = [
-  'blocking',
-];
-  page => {
-    console.log('page blocked - ' + page.url);
-
-    return {
-      cancel: true,
-    };
-  },
-  filter,
-  webRequestFlags,
-);
 chrome.webRequest.onBeforeRequest.addListener(
-    function() {
-        return {cancel: true};
-    },
-    {
-        urls: ["*://twitter.com/*", "*://facebook.com/*", "*://youtube.com/*"]
-    },
-    ["blocking"]
-);
-
-//BLOCK THE ENTIRE DOMAIN WITH THE FOLLOWING FUNCTION
-findAllURL = function changeAllURL(text){
-  var current = window.location.href;
-  if(current.startsWith(text)){
-    document.documentElement.innerHTML = '';
-    document.documentElement.innerHTML = 'Domain is blocked';
-    document.documentElement.scrollTop = 0;
-  }
-}
-
-
-findURL("https://www.twitter.com/");
-findAllURL("https://www.facebook.com/");
+    function(details) { return {cancel: true}; },
+    {urls: ["https://twitter.com"]},
+    ["blocking"]);
