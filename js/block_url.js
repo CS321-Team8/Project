c1f11a15ddc1e462cd07ec2f059055ad038adc71
cazurbach <59846812+cazurbach@users.noreply.github.com>
2020-11-18 14:34:53 -0500

    Update block_url.js

--- js/block_url.js
+++ js/block_url.js
@@ -1,17 +1,7 @@
-console.log("Loaded extension");
+const defaultFilters = ["*://*.twitter.com/*"]
 
-
-function blockRequest(details) {
-   return {cancel: true};
-}
-
-function updateFilters(urls) {
-   if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
-     chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
-   chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://*.facebook.com/*", "*://*.twitter.com/*"]}, ['blocking']);
-}
-updateFilters();
- chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return {cancel: true}; },
-    {urls: ["*://www.twitter.com/*"]},
-    ["blocking"]);
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true}},
+    { urls: defaultFilters },
+    ["blocking"]
+)
