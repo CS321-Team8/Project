93e42df3c05bd5bd6fd67c0cb65eb0b2830eee28
cazurbach <59846812+cazurbach@users.noreply.github.com>
2020-11-18 14:51:03 -0500

    Update block_url.js

--- js/block_url.js
+++ js/block_url.js
@@ -1,7 +1,4 @@
-const defaultFilters = ["*://*.twitter.com/*"]
-
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { return { cancel: true}},
-    { urls: defaultFilters },
-    ["blocking"]
-)
+      chrome.webRequest.onBeforeRequest.addListener(
+        function(details) { return {cancel: true}; },
+        {urls: ["*://www.twitter.com/*","*://www.facebook.com/*", "*://www.youtube.com/*"]},
+        ["blocking"]);
